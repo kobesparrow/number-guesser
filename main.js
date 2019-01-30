7e394721956ce3d8a3e4a907884a877c3a45c691
kobesparrow <markcollins.md@gmail.com>
2019-01-30 16:59:46 -0700

    initial JS

---
+++ main.js
@@ -0,0 +1,19 @@
+var minimumRange = document.querySelector('leftsection__range--minimum');
+
+minimumRange.addEventListener('click', function();
+
+
+
+function getUserName() {
+  var minRangeString = document.getElementByID('tktktk').value;
+  var minRangeNumber = parseInt(minRangeString);
+}
+
+// if (nameField.lenght <3) {
+//   result.textContent = 'Please select a smaller number';
+// }
+
+function clickButton() {
+  var dogName = document.getElementById('dog-name-input').value;
+  document.querySelector('.bold').innerText = dogName;
+}
