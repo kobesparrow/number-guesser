8771c91b104495e0798e3ab0e191fb476d7d0eb0
Aidan McKay <aidanmckay2000@gmail.com>
2019-02-01 09:44:06 -0700

    Making sure the JavaScript, CSS, and HTML  are completely in sync

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ var minRangeInput = document.querySelector('.column-left__range--highlowinputs')
 var maxRangeInput = document.querySelector('.column-left__range--highlowinputs');
 var updateButton = document.querySelector('.update-button');
 var randomNumber = generateRandom(minRange, maxRange);
-var displayLow = document.querySelector('.');
-var displayHigh = document.querySelector('.leftsection__users--highRange');
+var displayLow = document.querySelector('.displayLow');
+var displayHigh = document.querySelector('.displayHigh');
 var min = document.getElementById('min');
 var max = document.getElementById('max');
 
@@ -24,7 +24,7 @@ function generateRandom(min, max) {
 
 console.log(randomNumber);
 
-var playerTwoGuess = document.querySelector('.playerTwoGuess');
+var playerTwoGuess = document.querySelector('.value-input__guess');
 var submitGuessButton = document.querySelector('.submitGuess');
 var displayPlayerOneGuess = document.querySelector('.bigPinkPlayerOne')
 var displayPlayerTwoGuess = document.querySelector('.bigPinkPlayerTwo')
